c10341c074c6ef00e20abadafd85393e3d83f4b6
nxatdo <evans.tucker@dfinity.org>
2026-01-23 13:14:01 -0800

    Reverting accidental deploy

--- variables.sh
+++ variables.sh
@@ -4,9 +4,9 @@ export KUBECONFIG="${HOME}/.kube/project1-dev"
 export cluster_name=project1-dev
 export flux_path=flux
 export git_owner=devopscoop
-export git_platform=github
+export git_platform=gitlab
 export git_repo=project1-dev-deploy
-export k8s_platform=talos # eks, k0s, talos
+export k8s_platform=eks # eks, k0s, talos
 
 # Have to decrypt our encrypted keys.txt like this because of this bug:
 # https://github.com/getsops/sops/issues/933
